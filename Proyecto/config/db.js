cedc71b436c0e7c9e77d02e7101c9d0e30af8dd0
Alec Demian Santana Celaya <73375255+SantanaAlec@users.noreply.github.com>
2024-02-08 08:52:53 -0700

    prepare coding environment

---
+++ Proyecto/config/db.js
@@ -0,0 +1,10 @@
+//Module to instance Database conection
+const mysql = require('mysql2');
+require("dotenv").config({ path: "./variables.env" });
+const conection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+});
+
+module.exports = conection;
