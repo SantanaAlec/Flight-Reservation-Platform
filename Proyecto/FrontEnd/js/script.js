4e818c453dff1cd075b01dfea0b44917dae533ab
Alec Demian Santana Celaya <73375255+SantanaAlec@users.noreply.github.com>
2024-04-20 15:26:58 -0700

    Create header components

---
+++ Proyecto/FrontEnd/js/script.js
@@ -0,0 +1,5 @@
+import { HeaderComponent } from "./header/header.js";
+//import { FooterCompoent } from "./js/footer/footer.js";
+
+window.customElements.define("header-info", HeaderComponent);
+//window.customElements.define("footer-info", FooterCompoent);
